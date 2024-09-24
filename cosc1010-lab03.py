c6ea53ed655a537bdddc0eb9c4d408108a9b7f3a
nalsha62 <n.alshammari2005@gmail.com>
2024-09-24 01:31:58 +0000

    lab complete

--- cosc1010-lab03.py
+++ cosc1010-lab03.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Naser Alshammari
 # UWYO COSC 1010
-# Submission Date
+# 09/23/2024
 # Lab 03 
-# Lab Section: 
+# Lab Section: 13
 # Sources, people worked with, help given to: 
 # your
 # comments
@@ -15,46 +15,51 @@
 
 
 print("Part One------------------------------------------------------------------------")
-#We are going to start with the basics. Declare a list  states that contains the elements: Wyoming, Colorado, Montana in that order 
+#We are going to start with the basics. Declare a list  states that contains the elements:  in that Wyoming, Colorado, Montana order 
 #Note this is the ONLY point where you need to declare the states list
-
+states=['Wyoming', 'Colorado', 'Montana']
 
 
 #print the entire list
+print(states)
 
 
 #now print the first element in the list
-
+print(states[0])
 
 #Print the last element using the syntax shown in class to access the final element (hint, think negatives)
-
+print(states[-1])
 
 #Using an F-string to access the first and second element print the string "COLORADO is south of WYOMING", matching the casing provided
-
+print(f"{states[0].upper()} is south of{states[-1].upper()}")
 
 
 
 print("Part Two------------------------------------------------------------------------")
 #Append the following states to your list: Washington, Oregon, California and print your list
-
+states.append("Washington")
+states.append("Oregon")
+states.append("California")
 
 #Again using the specific syntax mentioned in class overwrite the second to last element to be Maine, printing the list 
-
+states[-2] = 'Maine'
+print(states)
 
 #Insert the state Texas to be the third element in the list, again printing your list
-
+states.insert(2, "Texas")
 
 #Using the `del` statement remove the fourth item from the list, print your list 
-
+del states[3]
 
 #Remove Texas using its value, print the list
-
+states.remove("Texas")
 print("Part Three----------------------------------------------------------------------")
 #Temporarily sort your list, print it both sorted and unsorted 
-
-
+print(sorted(states))
+ 
 #Permanently sort your list in reverse order, printing it out
-
-
+states.sort(reverse=True)
+print(states)
 #Using the reverse method reverse the list and print it
-
+states.reverse()
+print(states)
